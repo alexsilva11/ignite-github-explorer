8e2f86f196732122fb15163962989ef7e00fc690
alexsilva11 <alex.silva.11@hotmail.com>
2021-05-31 18:55:08 -0300

    Aplicacao Finalizada

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { RepositoryItem } from './RepositoryItem';
+
+import '../styles/repositories.scss'
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(res => res.json())
+    .then(data => setRepositories(data))
+  }, [])
+
+  console.log(repositories)
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+      {repositories.map(repository => (
+       <RepositoryItem key={repository.name} repository={repository} />
+      ))}
+      </ul>
+    </section>
+  )
+}
